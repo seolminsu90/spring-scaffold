f1f5d56f3d7c587e1a4135fafa2fea97d9fa0780
설민수 <minsu@nm-neo.com>
2022-08-25 13:52:52 +0900

    update routesource memo

--- src/main/java/com/admin/tool/common/config/db/RouteDataSource.java
+++ src/main/java/com/admin/tool/common/config/db/RouteDataSource.java
@@ -2,22 +2,21 @@ package com.admin.tool.common.config.db;
 
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
-import javax.sql.DataSource;
-import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class RouteDataSource extends AbstractRoutingDataSource {
+    // Master/Slave의 경우  determineCurrentLookupKey override에 ThreadLocal이 아니라
+    // TransactionSynchronizationManager.isCurrentTransactionReadOnly() 를 key lookup에 활용필요
+    // @Transactional(readOnly = true) 시 해당 값 전달받게 됨.
+    // ex) LookupKey => ThreadLocalContext.get() + { isMaster True/False ...}
     @Override
     protected Object determineCurrentLookupKey() {
         return ThreadLocalContext.get();
     }
 
-    public Set<Integer> getServers() {
-        Map<Object, DataSource> resolvedDataSources = super.getResolvedDataSources();
-        return resolvedDataSources.keySet()
-                .stream()
-                .map(key -> Integer.valueOf(key.toString()))
-                .collect(Collectors.toSet());
+
+    // 등록된 DataSource들의 Key 목록 가져옴
+    public Set<Object> getRegistServers() {
+        return super.getResolvedDataSources().keySet();
     }
 }
