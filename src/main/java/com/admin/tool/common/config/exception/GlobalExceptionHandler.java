64e50774c59e5f6cf7210e0ec19f636a1ecc21e6
설민수 <minsu@nm-neo.com>
2022-09-07 18:45:15 +0900

    update error stacktrace

--- src/main/java/com/admin/tool/common/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/admin/tool/common/config/exception/GlobalExceptionHandler.java
@@ -2,15 +2,19 @@ package com.admin.tool.common.config.exception;
 
 import com.admin.tool.common.model.ApiResponse;
 import com.admin.tool.common.model.Code;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger logger =  LoggerFactory.getLogger(this.getClass());
     // 종류별 추가
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ApiResponse<String>> handleException(final ApiException err) {
+        logger.error("Error ApiException");
 
         // 별도 추가 Body Data 처리
 
@@ -21,6 +25,9 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<String>> handleException(final Exception err) {
+        logger.error("Error Exception");
+
+        err.printStackTrace();
 
         return ResponseEntity
                 .internalServerError()
